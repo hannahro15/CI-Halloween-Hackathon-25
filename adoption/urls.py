e88dab32bd1e3faa5a0f2bdb53cf0d4a8a7058dd
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-10-30 22:50:42 +0000

    updated homepage and connect app to main urls

--- adoption/urls.py
+++ adoption/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.cat_adoption, name='cat-adoption'),
+    path('', include('adoptioncats.urls'))
 ]
