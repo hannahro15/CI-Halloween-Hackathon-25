ad54b85818ec99da2f3f785222b5d6d57c8d73f6
Hannah Olbrich <hannaholbrich@users.noreply.github.com>
2025-10-31 16:06:21 +0000

    Fix typo

--- adoptioncat/settings.py
+++ adoptioncat/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
     'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
