f77738c195ae47bc16bcf6d3b456ed20b2f19b86
SourTarte <dioncmcmillan@gmail.com>
2025-10-31 12:17:34 +0000

    Add: added speciality field to models.Cat, and added default values.
    Co-authored-by: Hannah Olbrich <hannahro15@users.noreply.github.com>

--- adoption/models.py
+++ adoption/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Cat(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    breed = models.CharField(max_length=100)
-    biography = models.TextField()
+    name = models.CharField(max_length=100, default="CatName")
+    age = models.IntegerField(default=1)
+    breed = models.CharField(max_length=100, default="CatBreed")
+    speciality = models.CharField(max_length=100, default="CatSpecial")
+    biography = models.TextField(max_length=400, default="CatBio")
     image = models.ImageField(upload_to='cat_images/')
 
 class CandidateList(models.Model):
