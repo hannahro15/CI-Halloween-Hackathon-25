1458b511e421dd4be64e165f9ab34dac85d845df
SourTarte <dioncmcmillan@gmail.com>
2025-10-31 12:56:43 +0000

    Add: Registered Cat model in admin panel

--- adoption/admin.py
+++ adoption/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Cat, CandidateList
 # Register your models here.
+@admin.register(Cat)
+class CatAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, filled for search.
+    Field filters, fields to prepopulate and rich-text editor
+    """
+    list_display = (
+        'id',
+        'name',
+        'age',
+        'speciality',
+    )
+    search_fields = ['name', 'speciality',]
+    list_filter = ('id', 'age',)
+    summernote_fields = ('biography',)
