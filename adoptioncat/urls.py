8da40ee034058850cb8da31c72eb39b792ad31c2
SourTarte <dioncmcmillan@gmail.com>
2025-10-31 13:13:00 +0000

    Add: Summernote reverse to project URLs

--- adoptioncat/urls.py
+++ adoptioncat/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include('adoption.urls'), name='adoption-urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
 ]
