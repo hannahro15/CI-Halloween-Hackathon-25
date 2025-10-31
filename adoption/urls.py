e134b140c4bf7c7e7720683045b1cf6af7826ca9
SourTarte <dioncmcmillan@gmail.com>
2025-10-31 14:32:42 +0000

    Add: Basic display of Cat model info to main-app.html

--- adoption/urls.py
+++ adoption/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cat_adoption, name='cat-adoption'),
+    #path('', views.cat_adoption, name='cat-adoption'),
+    path('', views.CatAdoption.as_view(), name='cat-adoption'),
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
