dba72ed11b27a57434b9edbd736bc63aad328bc4
Hannah Olbrich <hannaholbrich@users.noreply.github.com>
2025-11-01 10:37:00 +0000

    Fix paths and url links for home page, remove duplicate views file

--- adoption/urls.py
+++ adoption/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('team/', views.team_page, name='team-page'),
 ]
