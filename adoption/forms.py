2807accbf97ade4a2224caad640d57b8069a6f92
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-11-02 15:00:09 +0000

    Update forms.py to include some spooky styling

--- adoption/forms.py
+++ adoption/forms.py
@@ -1,8 +1,28 @@
 from django import forms
 from .models import ContactMessage
 
-
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ['name', 'email', 'message']
+        labels = {
+            'name': 'Your Witchy Name',
+            'email': 'Spirit Channel (Email)',
+            'message': 'Message to the Coven',
+        }
+        widgets = {
+            'message': forms.Textarea(attrs={'rows': 5}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': f'Enter your {field_name}...',
+            })
+
+        self.fields['message'].widget.attrs.update({
+            'placeholder': 'Type your mystical message here...',
+        })
